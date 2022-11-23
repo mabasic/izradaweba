790a2d8c4a7497188fa4fdfe8b42396fd4d7da9e
Mario Bašić <mario@laravelista.hr>
2022-11-23 09:52:30 +0100

    Uncomments mail files. cyclic reference issue.

--- src/main/scala/eu/izradaweba/mail/aws/v1/Ses.scala
+++ src/main/scala/eu/izradaweba/mail/aws/v1/Ses.scala
@@ -1,84 +1,84 @@
-// package eu.izradaweba.mail.aws.v1
+package eu.izradaweba.mail.aws.v1
 
-// import eu.izradaweba.Config
-// import eu.izradaweba.pages.ContactMessage
-// import eu.izradaweba.pages.subjects
-// import eu.izradaweba.validation.Email
+import eu.izradaweba.Config
+import eu.izradaweba.pages.ContactMessage
+import eu.izradaweba.pages.subjects
+import eu.izradaweba.validation.Email
 
-// import com.amazonaws.services.simpleemailv2.*
-// import com.amazonaws.auth.{
-//   AWSCredentials,
-//   AWSStaticCredentialsProvider,
-//   BasicAWSCredentials
-// }
-// import com.amazonaws.auth.profile.ProfileCredentialsProvider
-// import com.amazonaws.services.simpleemailv2.model.{
-//   Body,
-//   Content,
-//   Destination,
-//   Message,
-//   SendEmailRequest,
-//   EmailContent,
-//   SendEmailResult
-// }
+import com.amazonaws.services.simpleemailv2.*
+import com.amazonaws.auth.{
+  AWSCredentials,
+  AWSStaticCredentialsProvider,
+  BasicAWSCredentials
+}
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.services.simpleemailv2.model.{
+  Body,
+  Content,
+  Destination,
+  Message,
+  SendEmailRequest,
+  EmailContent,
+  SendEmailResult
+}
 
-// val credentials = BasicAWSCredentials(
-//   Config.ses.key,
-//   Config.ses.secret
-// )
+val credentials = BasicAWSCredentials(
+  Config.ses.key,
+  Config.ses.secret
+)
 
-// val client: AmazonSimpleEmailServiceV2 = AmazonSimpleEmailServiceV2ClientBuilder
-//   .standard()
-//   .withCredentials(AWSStaticCredentialsProvider(credentials))
-//   .withRegion(Config.ses.region)
-//   .build
+val client: AmazonSimpleEmailServiceV2 = AmazonSimpleEmailServiceV2ClientBuilder
+  .standard()
+  .withCredentials(AWSStaticCredentialsProvider(credentials))
+  .withRegion(Config.ses.region)
+  .build
 
-// def send(
-//     to: String,
-//     subject0: String,
-//     body0: String,
-//     replyTo: Email
-// ): SendEmailResult =
-//   val subject: Content = Content().withData(subject0)
+def send(
+    to: String,
+    subject0: String,
+    body0: String,
+    replyTo: Email
+): SendEmailResult =
+  val subject: Content = Content().withData(subject0)
 
-//   val bodyText: Content = Content().withData(body0)
-//   val body: Body = Body().withText(bodyText)
+  val bodyText: Content = Content().withData(body0)
+  val body: Body = Body().withText(bodyText)
 
-//   val message: Message = Message().withBody(body).withSubject(subject)
+  val message: Message = Message().withBody(body).withSubject(subject)
 
-//   val content: EmailContent = EmailContent().withSimple(message)
+  val content: EmailContent = EmailContent().withSimple(message)
 
-//   val destination: Destination =
-//     Destination().withToAddresses(to)
+  val destination: Destination =
+    Destination().withToAddresses(to)
 
-//   val request: SendEmailRequest = SendEmailRequest()
-//     .withDestination(destination)
-//     .withContent(content)
-//     .withReplyToAddresses(replyTo)
+  val request: SendEmailRequest = SendEmailRequest()
+    .withDestination(destination)
+    .withContent(content)
+    .withReplyToAddresses(replyTo)
 
-//   request.setFromEmailAddress(Config.replyToAddress)
+  request.setFromEmailAddress(Config.replyToAddress)
 
-//   client.sendEmail(request)
+  client.sendEmail(request)
 
-// def sendContactMessage(message: ContactMessage): SendEmailResult =
-//   val emailSubject = "Poruka sa web stranice | IzradaWeba"
+def sendContactMessage(message: ContactMessage): SendEmailResult =
+  val emailSubject = "Poruka sa web stranice | IzradaWeba"
 
-//   val subject = subjects.find(_.id == message.subject.tag) match
-//     case Some(subject) => subject.text
-//     case None          => message.subject.tag
+  val subject = subjects.find(_.id == message.subject.tag) match
+    case Some(subject) => subject.text
+    case None          => message.subject.tag
 
-//   val body = s"""Ime i prezime: ${message.full_name}
-//                 |Email: ${message.email_address}
-//                 |Predmet: ${subject}
-//                 |Poruka:
-//                 |
-//                 |${message.message}
-//                 |
-//                 |*Korisnik je dao svoju privolu da ga se može kontaktirati u vezi ove poruke.""".stripMargin
+  val body = s"""Ime i prezime: ${message.full_name}
+                |Email: ${message.email_address}
+                |Predmet: ${subject}
+                |Poruka:
+                |
+                |${message.message}
+                |
+                |*Korisnik je dao svoju privolu da ga se može kontaktirati u vezi ove poruke.""".stripMargin
 
-//   send(
-//     to = Config.emailAddress,
-//     subject0 = emailSubject,
-//     body0 = body,
-//     replyTo = message.email_address
-//   )
+  send(
+    to = Config.emailAddress,
+    subject0 = emailSubject,
+    body0 = body,
+    replyTo = message.email_address
+  )
